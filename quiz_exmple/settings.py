d8096aee95f46b0fdbe730dce04b55d3ee889640
Eswar <eswar@cowhite.com>
2016-10-10 23:14:22 +0530

    Added whitenoise.

--- quiz_exmple/settings.py
+++ quiz_exmple/settings.py
@@ -143,3 +143,8 @@ STATICFILES_DIRS = (
 # Update database configuration with $DATABASE_URL.
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
