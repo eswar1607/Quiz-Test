5d656cc4e6b5aec0fa6d010ca71c0352dbb8d591
Eswar <eswar@cowhite.com>
2016-10-11 15:56:58 +0530

    Fixes.

--- quiz_exmple/settings.py
+++ quiz_exmple/settings.py
@@ -146,7 +146,7 @@ STATICFILES_DIRS = (
     (os.path.join(BASE_DIR, "staticfiles"),)
 )
 # Update database configuration with $DATABASE_URL.
-db_from_env = dj_database_url.config()
+db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
 # Simplified static file serving.
--- quiz_exmple/wsgi.py
+++ quiz_exmple/wsgi.py
@@ -10,10 +10,7 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
-
-application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "quiz_exmple.settings")
 
+application = get_wsgi_application()
